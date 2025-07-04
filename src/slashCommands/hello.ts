606340b7a5858938664b34bada06b78f2e51499a
Glenn Marten <59699274+ToastBoxMe@users.noreply.github.com>
2025-07-04 23:31:35 +0800

    Update hello.ts

--- src/slashCommands/hello.ts
+++ src/slashCommands/hello.ts
@@ -1,13 +1,11 @@
-// src/slashCommands/hello.ts
 
 import { SlashCommandBuilder } from 'discord.js';
 import { SlashCommand } from '../types';
 
 const command: SlashCommand = {
-  ...new SlashCommandBuilder()
+  data: new SlashCommandBuilder()
     .setName('hello')
-    .setDescription('Get information about this bot')
-    .toJSON(), // Convert builder to raw data object
+    .setDescription('Get information about this bot'),
   async execute(interaction) {
     await interaction.reply('This bot is a helpful bot.');
   },
