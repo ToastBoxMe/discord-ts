084769e3248db1f62139fe8e22ed8a42d852ea35
Glenn Marten <59699274+ToastBoxMe@users.noreply.github.com>
2025-07-04 23:51:34 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,67 @@
-import { Client, Collection, Events, GatewayIntentBits } from 'discord.js';
-import { config } from 'dotenv';
-import { token } from './config';
-import testCommand from './slashCommands/ping';
-import { SlashCommand } from './types';
-import fs from 'node:fs';
-import path from 'node:path';
+import {
+    Client,
+    Events,
+    GatewayIntentBits,
+    SlashCommandBuilder,
+    Partials,
+    REST,
+    Routes,
+    Collection
+} from "discord.js";
+import type { SlashCommand } from "./types";
+import { join } from "path";
+import { readdirSync } from "fs";
+import dotenv from "dotenv";
+dotenv.config();
+import testCommand from "./slashCommands/ping";
 
-config();
+const token = process.env.DISCORD_TOKEN; // Token from Railway Env Variable.
+const client_id = process.env.CLIENT_ID;
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.MessageContent,
+    ],
+    partials: [Partials.Channel],
+});
+client.once(Events.ClientReady, async (c) => {
+    console.log(`Logged in as ${c.user.tag}`);
+});
+console.log("jweqioweqeqww");
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-client.commands = new Collection();
+const slashCommands = new Collection<string, SlashCommand>()
+slashCommands.set(testCommand.command.name, testCommand)
+const slashCommandsArr: SlashCommandBuilder[] = [testCommand.command]
 
-const slashCommandsArr = [];
-slashCommandsArr.push(testCommand.data.toJSON());
-
-client.commands.set(testCommand.data.name, testCommand);
-
-const commandsPath = path.join(__dirname, 'slashCommands');
-const commandFiles = fs
-  .readdirSync(commandsPath)
-  .filter(file => file.endsWith('.ts') || file.endsWith('.js'));
-
-for (const file of commandFiles) {
-  const filePath = path.join(commandsPath, file);
-  const command = require(filePath).default as SlashCommand;
-
-  if ('data' in command && 'execute' in command) {
-    client.commands.set(command.data.name, command);
-    slashCommandsArr.push(command.data.toJSON());
-  } else {
-    console.log(
-      `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
-    );
-  }
-}
-
-client.once(Events.ClientReady, c => {
-  console.log(`Ready! Logged in as ${c.user.tag}`);
+const rest = new REST({ version: "10" }).setToken(token);
+rest.put(Routes.applicationCommands(client_id), {
+    body: slashCommandsArr.map(command => command.toJSON())
+}).then((data: any) => {
+    console.log(`🔥 Successfully loaded ${data.length} slash command(s)`)
+}).catch(e => {
+    console.log(e)
 });
 
 client.on(Events.InteractionCreate, async interaction => {
-  if (!interaction.isChatInputCommand()) return;
+    if (!interaction.isChatInputCommand()) return;
+    const command = slashCommands.get(interaction.commandName);
 
-  const command = client.commands.get(interaction.commandName);
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
 
-  if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found.`);
-    return;
-  }
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    await interaction.reply({
-      content: 'There was an error while executing this command!',
-      ephemeral: true,
-    });
-  }
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+        } else {
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
+    }
 });
-
-client.login(token);
+client
+    .login(token)
+    .catch((error) => console.error("Discord.Client.Login.Error", error));
