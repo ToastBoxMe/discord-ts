c731266df147c755c6af2aaea5000bf20369ddf8
Glenn Marten <59699274+ToastBoxMe@users.noreply.github.com>
2025-07-04 23:46:05 +0800

    Update ping.ts

--- src/slashCommands/ping.ts
+++ src/slashCommands/ping.ts
@@ -1,63 +1,21 @@
-import { Client, Collection, Events, GatewayIntentBits } from 'discord.js';
-import { config } from 'dotenv';
-import { token } from './config';
-import testCommand from './slashCommands/ping';
-import { SlashCommand } from './types';
-import fs from 'node:fs';
-import path from 'node:path';
+import { SlashCommandBuilder } from 'discord.js';
+import { SlashCommand } from '../types';
 
-config();
-
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-client.commands = new Collection();
-
-const slashCommandsArr = [];
-slashCommandsArr.push(testCommand.data.toJSON());
-
-client.commands.set(testCommand.data.name, testCommand);
-
-const commandsPath = path.join(__dirname, 'slashCommands');
-const commandFiles = fs
-  .readdirSync(commandsPath)
-  .filter(file => file.endsWith('.ts') || file.endsWith('.js'));
-
-for (const file of commandFiles) {
-  const filePath = path.join(commandsPath, file);
-  const command = require(filePath).default as SlashCommand;
-
-  if ('data' in command && 'execute' in command) {
-    client.commands.set(command.data.name, command);
-    slashCommandsArr.push(command.data.toJSON());
-  } else {
-    console.log(
-      `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
-    );
-  }
-}
-
-client.once(Events.ClientReady, c => {
-  console.log(`Ready! Logged in as ${c.user.tag}`);
-});
-
-client.on(Events.InteractionCreate, async interaction => {
-  if (!interaction.isChatInputCommand()) return;
-
-  const command = client.commands.get(interaction.commandName);
-
-  if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found.`);
-    return;
-  }
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    await interaction.reply({
-      content: 'There was an error while executing this command!',
-      ephemeral: true,
+const command: SlashCommand = {
+  data: new SlashCommandBuilder()
+    .setName('test')
+    .setDescription('Replies with Pong!'),
+  async execute(interaction) {
+    const sent = await interaction.reply({
+      content: 'Pong!',
+      fetchReply: true,
     });
-  }
-});
+    interaction.editReply(
+      `Roundtrip latency: ${
+        sent.createdTimestamp - interaction.createdTimestamp
+      }ms`,
+    );
+  },
+};
 
-client.login(token);
+export default command;
