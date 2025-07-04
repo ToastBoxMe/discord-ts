c2673540bf865a8c4812783b7724ec1c27c33d57
Glenn Marten <59699274+ToastBoxMe@users.noreply.github.com>
2025-07-04 17:51:24 +0800

    Update ping.ts

--- src/slashCommands/ping.ts
+++ src/slashCommands/ping.ts
@@ -30,26 +30,5 @@ const testCommand: SlashCommand = {
     },
     cooldown: 3
 }
-const testPingPong: SlashCommand = {
-    command: new SlashCommandBuilder()
-        .setName("ping")
-        .setDescription("reply pong")
-        .addStringOption(option => {
-            return option
-                .setName("content")
-                .setDescription("this is a parameter for a command")
-                .setRequired(false);
-        }),
-    execute: async (interaction) => {
-        const options: { [key: string]: string | number | boolean } = {};
-        for (let i = 0; i < interaction.options.data.length; i++) {
-            const element = interaction.options.data[i];
-            if (element.name && element.value) options[element.name] = element.value;
-        }
 
-        interaction.reply('Pong!');
-    },
-    cooldown: 3
-}
-export default testPingPong;
 export default testCommand;
