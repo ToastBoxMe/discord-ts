26ed20d442445444611f8d8dba995aa0d24edce8
Glenn Marten <59699274+ToastBoxMe@users.noreply.github.com>
2025-07-05 00:37:42 +0800

    Update hello.ts

--- src/slashCommands/hello.ts
+++ src/slashCommands/hello.ts
@@ -1,34 +1,14 @@
-import { SlashCommandBuilder, ChannelType, TextChannel, EmbedBuilder } from "discord.js"
-import { SlashCommand } from "../types";
+import { SlashCommandBuilder } from 'discord.js';
+import { SlashCommand } from '../types';
 
 const hellocommand: SlashCommand = {
-    command: new SlashCommandBuilder()
-        .setName("hello")
-        .setDescription("Test command")
-        .addStringOption(option => {
-            return option
-                .setName("content")
-                .setDescription("this is a parameter for a command")
-                .setRequired(false);
-        }),
-    execute: async (interaction) => {
-        const options: { [key: string]: string | number | boolean } = {};
-        for (let i = 0; i < interaction.options.data.length; i++) {
-            const element = interaction.options.data[i];
-            if (element.name && element.value) options[element.name] = element.value;
-        }
+  data: new SlashCommandBuilder()
+    .setName('hello')
+    .setDescription('Get information about this bot'),
+  async execute(interaction) {
+    await interaction.reply('This bot is a helpful bot.');
+  },
+};
 
-        interaction.reply({
-            embeds: [
-                new EmbedBuilder()
-                    .setAuthor({ name: "Response Title" })
-                    .setDescription(`👋 Hi! 
-                    Your ping: ${interaction.client.ws.ping}
-                    Your input: ${options.content}`)
-            ]
-        })
-    },
-    cooldown: 3
-}
 
 export default hellocommand;
