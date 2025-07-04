9c479b3bda979d3aaf7517d9d5c35a582003773d
Glenn Marten <59699274+ToastBoxMe@users.noreply.github.com>
2025-07-04 23:30:11 +0800

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -5,9 +5,11 @@ export interface SlashCommand {
     execute: (interaction : CommandInteraction) => void,
     autocomplete?: (interaction: AutocompleteInteraction) => void,
     cooldown?: number // in seconds
-*/}
+}
+*/
 import { ChatInputApplicationCommandData, CommandInteraction } from 'discord.js';
 
 export interface SlashCommand extends ChatInputApplicationCommandData {
   execute: (interaction: CommandInteraction) => Promise<void>;
 }
+
