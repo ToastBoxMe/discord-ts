1f1b88c5845bb57bdfb403ec5a60d63b9b598e8f
Glenn Marten <59699274+ToastBoxMe@users.noreply.github.com>
2025-07-04 23:50:53 +0800

    Update ping.ts

--- src/slashCommands/ping.ts
+++ src/slashCommands/ping.ts
@@ -1,21 +1,34 @@
-import { SlashCommandBuilder } from 'discord.js';
-import { SlashCommand } from '../types';
+import { SlashCommandBuilder, ChannelType, TextChannel, EmbedBuilder } from "discord.js"
+import { SlashCommand } from "../types";
 
-const command: SlashCommand = {
-  data: new SlashCommandBuilder()
-    .setName('test')
-    .setDescription('Replies with Pong!'),
-  async execute(interaction) {
-    const sent = await interaction.reply({
-      content: 'Pong!',
-      fetchReply: true,
-    });
-    interaction.editReply(
-      `Roundtrip latency: ${
-        sent.createdTimestamp - interaction.createdTimestamp
-      }ms`,
-    );
-  },
-};
+const testCommand: SlashCommand = {
+    command: new SlashCommandBuilder()
+        .setName("test")
+        .setDescription("Test command")
+        .addStringOption(option => {
+            return option
+                .setName("content")
+                .setDescription("this is a parameter for a command")
+                .setRequired(false);
+        }),
+    execute: async (interaction) => {
+        const options: { [key: string]: string | number | boolean } = {};
+        for (let i = 0; i < interaction.options.data.length; i++) {
+            const element = interaction.options.data[i];
+            if (element.name && element.value) options[element.name] = element.value;
+        }
 
-export default command;
+        interaction.reply({
+            embeds: [
+                new EmbedBuilder()
+                    .setAuthor({ name: "Response Title" })
+                    .setDescription(`👋 Hi! 
+                    Your ping: ${interaction.client.ws.ping}
+                    Your input: ${options.content}`)
+            ]
+        })
+    },
+    cooldown: 3
+}
+
+export default testCommand;
