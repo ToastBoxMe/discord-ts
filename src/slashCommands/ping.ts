397e7c1b8ec2298453a86de6f07765a5206a9773
Glenn Marten <59699274+ToastBoxMe@users.noreply.github.com>
2025-07-04 23:45:34 +0800

    Update ping.ts

--- src/slashCommands/ping.ts
+++ src/slashCommands/ping.ts
@@ -1,34 +1,63 @@
-import { SlashCommandBuilder, ChannelType, TextChannel, EmbedBuilder } from "discord.js"
-import { SlashCommand } from "../types";
+import { Client, Collection, Events, GatewayIntentBits } from 'discord.js';
+import { config } from 'dotenv';
+import { token } from './config';
+import testCommand from './slashCommands/ping';
+import { SlashCommand } from './types';
+import fs from 'node:fs';
+import path from 'node:path';
 
-const testCommand: SlashCommand = {
-    command: new SlashCommandBuilder()
-        .setName("test")
-        .setDescription("Test command")
-        .addStringOption(option => {
-            return option
-                .setName("content")
-                .setDescription("this is a parameter for a command")
-                .setRequired(false);
-        }),
-    execute: async (interaction) => {
-        const options: { [key: string]: string | number | boolean } = {};
-        for (let i = 0; i < interaction.options.data.length; i++) {
-            const element = interaction.options.data[i];
-            if (element.name && element.value) options[element.name] = element.value;
-        }
+config();
 
-        interaction.reply({
-            embeds: [
-                new EmbedBuilder()
-                    .setAuthor({ name: "Response Title" })
-                    .setDescription(`👋 test! 
-                    Your ping: ${interaction.client.ws.ping}
-                    Your input: ${options.content}`)
-            ]
-        })
-    },
-    cooldown: 3
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.commands = new Collection();
+
+const slashCommandsArr = [];
+slashCommandsArr.push(testCommand.data.toJSON());
+
+client.commands.set(testCommand.data.name, testCommand);
+
+const commandsPath = path.join(__dirname, 'slashCommands');
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter(file => file.endsWith('.ts') || file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath).default as SlashCommand;
+
+  if ('data' in command && 'execute' in command) {
+    client.commands.set(command.data.name, command);
+    slashCommandsArr.push(command.data.toJSON());
+  } else {
+    console.log(
+      `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
+    );
+  }
 }
 
-export default testCommand;
+client.once(Events.ClientReady, c => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: 'There was an error while executing this command!',
+      ephemeral: true,
+    });
+  }
+});
+
+client.login(token);
